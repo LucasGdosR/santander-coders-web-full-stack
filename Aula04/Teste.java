7c6c9c2af42d6ce701f43a188fcdc56c5bb47d26
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2022-11-17 09:00:53 -0300

    entrega classe 'teste'

---
+++ Aula04/Teste.java
@@ -0,0 +1,12 @@
+public class Teste {
+    private int idade;
+    private String resultado;
+
+    Teste(int idade, String resultado){
+        this.idade = idade;
+        this.resultado = resultado;
+    }
+
+    public int getIdade() { return idade; }
+    public String getResultado() { return resultado; }
+}
